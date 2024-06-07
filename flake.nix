312b675323d7c5cf565d236c8d21ff1e6a3741fe
Beaverr <theboxguy659@gmail.com>
2024-06-07 11:01:53 -0500

    hold

--- flake.nix
+++ flake.nix
@@ -3,21 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-        nixosConfigurations = {
+    nixosConfigurations = {
       toaster-oven-of-death = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./sys/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.backupFileExtension = "backup";
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.beaver = import ./home/home.nix;
+          }
         ];
       };
     };
-};
+  };
 }
