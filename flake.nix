45ba6530a73c8a00f60a188b7c0aa148617ad278
Beaverr <theboxguy659@gmail.com>
2024-06-07 11:06:16 -0500

    re rename because I realize I just forgot to change an import

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       toaster-oven-of-death = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./sys/configuration.nix
+          ./nixos/configuration.nix
 
           home-manager.nixosModules.home-manager
           {
