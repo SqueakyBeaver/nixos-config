0cf909a1826a0515295665bf271e9f7d20134bb0
Beaverr <theboxguy659@gmail.com>
2024-06-19 12:48:39 -0500

    Fix hibernation

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -23,6 +23,10 @@ in {
     size = 8 * 1024;
   }];
 
+  boot.resumeDevice = "/dev/disk/by-partlabel/root";
+  # FIXME: Change if drive chages or anything
+  boot.kernelParams = [ "resume_offset=23279616" ];
+
 
   networking.hostName = "toaster-oven-of-death";
 }
