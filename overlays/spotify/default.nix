525f7ec89dd421af6c0f49d658aaf9b7cef77b03
Beaverr <beaver@transgender.army>
2024-07-30 21:14:09 -0500

    Fix spotx overlay

--- overlays/spotify/default.nix
+++ overlays/spotify/default.nix
@@ -4,16 +4,30 @@
   ...
 }: final: prev: {
   spotify = prev.spotify.overrideAttrs (finalAttrs: prevAttrs: {
+    spotx = prev.fetchurl {
+      url = "https://raw.githubusercontent.com/SpotX-Official/SpotX-Bash/main/spotx.sh";
+      hash = "sha256-AGTxNbR+jAi7X5DQYyi2UfEYPg71sljO6S4PQH0hypA=   ";
+    };
     installPhase =
       builtins.replaceStrings [
         "runHook postInstall"
       ]
       [
         ''
-          bash <(curl -sSL https://spotx-official.github.io/run.sh) -P "$out/share/spotify"
+          bash $spotx -P "$out/share/spotify"
           runHook postInstall
         ''
       ]
       prevAttrs.installPhase;
+
+    nativeBuildInputs =
+      prevAttrs.nativeBuildInputs
+      ++ [
+        final.curl
+        final.perl
+        final.util-linux
+        final.unzip
+        final.zip
+      ];
   });
 }
