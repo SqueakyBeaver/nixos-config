a5c61296e1b85cce8a5975f2c99f628148db3eae
Beaverr <beaver@transgender.army>
2024-11-18 19:17:03 -0600

    Maybe fix thinkpad thing

---
+++ homes/x86_64-linux/beaver@thinkpad/default.nix
@@ -0,0 +1,19 @@
+{...}: {
+  timber = {
+    browser.enable = true;
+    editors.enable = true;
+    terminal.enable = true;
+
+    apps = {
+      bitwarden.enable = true;
+      graphics.enable = false;
+      obsidian.enable = false;
+      prismlauncher.enable = true;
+      spotify.enable = true;
+      vesktop.enable = true;
+      vlc.enable = true;
+      libreoffice.enable = true;
+      # pixelflasher.enable = true;
+    };
+  };
+}
