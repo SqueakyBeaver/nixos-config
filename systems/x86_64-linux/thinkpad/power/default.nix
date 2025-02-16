8f5210cb73ba580a5cbc77a0392b842c83d819a2
Beaverr <beaver@transgender.army>
2025-02-16 10:33:30 -0600

    Disable tlp

--- systems/x86_64-linux/thinkpad/power/default.nix
+++ systems/x86_64-linux/thinkpad/power/default.nix
@@ -9,22 +9,24 @@
     thinkfan.enable = true;
 
     upower.enable = true;
+    # nix-hardware enables tlp and I don't want it to >:(
+    tlp.enable = false;
 
     # Just in case kde plasma enables it
     power-profiles-daemon.enable = false;
   };
 
   programs.auto-cpufreq = {
-      enable = true;
-      settings = {
-        charger = {
-          governor = "performance";
-          turbo = "auto";
-        };
-        battery = {
-          governor = "powersave";
-          turbo = "never";
-        };
+    enable = true;
+    settings = {
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+      battery = {
+        governor = "powersave";
+        turbo = "never";
       };
     };
+  };
 }
