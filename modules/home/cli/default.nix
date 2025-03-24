6d998143447d346ae075c61c21b3d9bcebec6fa3
Beaverr <beaver@transgender.army>
2025-03-24 13:45:25 -0500

    Git settings tweaks

--- modules/home/cli/default.nix
+++ modules/home/cli/default.nix
@@ -34,10 +34,69 @@
       # FIXME: If you copy this, change it
       userName = "Beaverr";
       userEmail = "beaver@transgender.army"; # hehe funnee email
+
+      # https://blog.gitbutler.com/how-git-core-devs-configure-git/
+      extraConfig = {
+        column = {
+          ui = "auto";
+        };
+        branch = {
+          sort = "-committerdate";
+        };
+        tag = {
+          sort = "version:refname";
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        diff = {
+          algorithm = "histogram";
+          colorMoved = "plain";
+          mnemonicPrefix = true;
+          renames = true;
+        };
+        push = {
+          default = "simple";
+          autoSetupRemote = true;
+          followTags = true;
+        };
+        fetch = {
+          prune = true;
+          pruneTags = true;
+          all = true;
+        };
+
+        # Why the hell not?
+        help = {
+          autocorrect = "prompt";
+        };
+        commit = {
+          verbose = true;
+        };
+        rerere = {
+          enabled = true;
+          autoupdate = true;
+        };
+        core = {
+          excludesfile = "~/.gitignore";
+          # fsmonitor = true; # Personal taste
+          # untrackedCache = true; # Personal taste
+        };
+        rebase = {
+          autoSquash = true;
+          autoStash = true;
+          updateRefs = true;
+        };
+        merge = {
+          conflictstyle = "zdiff3"; # Personal taste
+        };
+        pull = {
+          rebase = true; # Personal taste
+        };
+      };
     };
   };
 
-  
   home.file = {
     ".ssh/config" = {
       target = ".ssh/config_source";
