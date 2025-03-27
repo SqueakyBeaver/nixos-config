093988012a503d0c9fe173a92e54b766ae544d86
Beaverr <squeaky.beaver4133@protonmail.com>
2025-03-27 14:14:18 -0500

    Use vscode-fhs because I'm tired of fiddling with it tbh

--- modules/home/editors/default.nix
+++ modules/home/editors/default.nix
@@ -47,79 +47,85 @@ in {
   };
 
   config = mkIf cfg.enable {
-    programs.vscode = let
-      vscode-extensions = inputs.nix-vscode-extensions.extensions.${system};
-    in
-      mkIf cfg.codium.enable {
-        enable = true;
-        # package = pkgs.vscodium;
-        profiles.default = {
-          extensions = with vscode-extensions;
-          with pkgs.vscode-extensions;
-            [
-              open-vsx.rust-lang.rust-analyzer
-              open-vsx.mkhl.direnv
-              open-vsx.jnoortheen.nix-ide
-              open-vsx.tamasfe.even-better-toml
-              open-vsx.serayuzgur.crates
-              open-vsx.pkief.material-icon-theme
-              open-vsx.eamodio.gitlens
-            ]
-            ++ [
-              # nixpkgs vscode-extensions
-              # For those extensions that  don't work on the other vscode-extensions repo
-              vadimcn.vscode-lldb
-              ms-python.python
-              ms-python.debugpy
-              ms-python.isort
-              ms-python.vscode-pylance
-              ms-python.black-formatter
-              visualstudioexptteam.vscodeintellicode
-              yzhang.markdown-all-in-one
-              shd101wyy.markdown-preview-enhanced
-              llvm-vs-code-extensions.vscode-clangd
-              ms-vscode.cpptools-extension-pack
-              ms-toolsai.jupyter
-              ms-toolsai.jupyter-renderers
-              ms-toolsai.datawrangler
-              ms-vscode-remote.remote-ssh
-              ms-vscode-remote.remote-ssh-edit
-            ];
+    programs.vscode = {
+      enable = true;
+      package = pkgs.vscode-fhs;
+    };
+    # programs.vscode = let
+    #   vscode-extensions = inputs.nix-vscode-extensions.extensions.${system};
+    # in
+    #   mkIf cfg.codium.enable {
+    #     enableExtensionUpdateCheck = false;
+    #     enableUpdateCheck = false;
+    #     enable = true;
+    #     # package = pkgs.vscodium;
+    #     profiles.default = {
+    #       extensions = with vscode-extensions;
+    #       with pkgs.vscode-extensions;
+    #         [
+    #           open-vsx.rust-lang.rust-analyzer
+    #           open-vsx.mkhl.direnv
+    #           open-vsx.jnoortheen.nix-ide
+    #           open-vsx.tamasfe.even-better-toml
+    #           open-vsx.serayuzgur.crates
+    #           open-vsx.pkief.material-icon-theme
+    #           open-vsx.eamodio.gitlens
+    #         ]
+    #         ++ [
+    #           # nixpkgs vscode-extensions
+    #           # For those extensions that  don't work on the other vscode-extensions repo
+    #           vadimcn.vscode-lldb
+    #           ms-python.python
+    #           ms-python.debugpy
+    #           ms-python.isort
+    #           ms-python.vscode-pylance
+    #           ms-python.black-formatter
+    #           visualstudioexptteam.vscodeintellicode
+    #           yzhang.markdown-all-in-one
+    #           shd101wyy.markdown-preview-enhanced
+    #           llvm-vs-code-extensions.vscode-clangd
+    #           ms-vscode.cpptools-extension-pack
+    #           ms-toolsai.jupyter
+    #           ms-toolsai.jupyter-renderers
+    #           ms-toolsai.datawrangler
+    #           ms-vscode-remote.remote-ssh
+    #           ms-vscode-remote.remote-ssh-edit
+    #         ];
 
-          userSettings = {
-            "workbench.colorTheme" = "Default Dark+";
-            "editor.fontFamily" = "Fira Code";
-            "files.autoSave" = "afterDelay";
-            "editor.bracketPairColorization.independentColorPoolPerBracketType" = true;
-            "editor.fontLigatures" = true;
-            "editor.minimap.enabled" = false;
-            "editor.formatOnSave" = true;
-            "nix.enableLanguageServer" = true;
-            "nix.formatterPath" = "alejandra";
-            "rust-analyzer.debug.openDebugPane" = true;
-            "workbench.iconTheme" = "material-icon-theme";
-            "nix.serverSettings" = {
-              "nil" = {
-                "diagnostics" = {
-                  "ignored" = ["unused_binding" "unused_with"];
-                };
-                "formatting" = {
-                  "command" = ["alejandra"];
-                };
-              };
-            };
-            # "python.languageServer" = "Jedi";
-            "gitlens.telemetry.enabled" = false;
-            "telemetry.telemetryLevel" = "off";
-            "leetcode.workspaceFolder" = "/home/beaver/Coding/leetcode";
-            "leetcode.defaultLanguage" = "python3";
-            "leetcode.hint.commandShortcut" = false;
-            "leetcode.hint.setDefaultLanguage" = false;
-            "leetcode.hint.configWebviewMarkdown" = false;
-            "leetcode.showDescription" = "Both";
-          };
-        };
-      };
+    #       userSettings = {
+    #         "workbench.colorTheme" = "Default Dark+";
+    #         "editor.fontFamily" = "Fira Code";
+    #         "files.autoSave" = "afterDelay";
+    #         "editor.bracketPairColorization.independentColorPoolPerBracketType" = true;
+    #         "editor.fontLigatures" = true;
+    #         "editor.minimap.enabled" = false;
+    #         "editor.formatOnSave" = true;
+    #         "nix.enableLanguageServer" = true;
+    #         "nix.formatterPath" = "alejandra";
+    #         "rust-analyzer.debug.openDebugPane" = true;
+    #         "workbench.iconTheme" = "material-icon-theme";
+    #         "nix.serverSettings" = {
+    #           "nil" = {
+    #             "diagnostics" = {
+    #               "ignored" = ["unused_binding" "unused_with"];
+    #             };
+    #             "formatting" = {
+    #               "command" = ["alejandra"];
+    #             };
+    #           };
+    #         };
+    #         # "python.languageServer" = "Jedi";
+    #         "gitlens.telemetry.enabled" = false;
+    #         "telemetry.telemetryLevel" = "off";
+    #         "leetcode.workspaceFolder" = "/home/beaver/Coding/leetcode";
+    #         "leetcode.defaultLanguage" = "python3";
+    #         "leetcode.hint.commandShortcut" = false;
+    #         "leetcode.hint.setDefaultLanguage" = false;
+    #         "leetcode.hint.configWebviewMarkdown" = false;
+    #         "leetcode.showDescription" = "Both";
+    #       };
+    #     };
+    #   };
 
     home.packages = [
       (mkIf cfg.android.enable pkgs.android-studio)
